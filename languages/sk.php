7eff36c398d84a364fc70b2f60c5ae0a733132a8
Christoph M. Becker <cmbecker69@gmx.de>
2017-04-15 19:02:57 +0200

    Really add the Slovak translation
    We claimed to have done that with commit 513af9b, but obviously we
    forgot to actually add the relevant files.

---
+++ languages/sk.php
@@ -0,0 +1,21 @@
+<?php
+
+$plugin_tx['foldergallery']['locator_start']="Štart";
+$plugin_tx['foldergallery']['locator_separator']=" > ";
+$plugin_tx['foldergallery']['colorbox_current']="Obrázok {current} z {total}";
+$plugin_tx['foldergallery']['colorbox_previous']="predošlý";
+$plugin_tx['foldergallery']['colorbox_next']="ďalší";
+$plugin_tx['foldergallery']['colorbox_close']="zatvoriť";
+$plugin_tx['foldergallery']['colorbox_imgError']="Tento obrázok sa nepodarilo načítať.";
+$plugin_tx['foldergallery']['alt_logo']="Prázdny adresár";
+$plugin_tx['foldergallery']['syscheck_extension']="PHP rozšírenie '%s' aktívne";
+$plugin_tx['foldergallery']['syscheck_fail']="chyba";
+$plugin_tx['foldergallery']['syscheck_message']="Kontrola systému %1\$s … %2\$s";
+$plugin_tx['foldergallery']['syscheck_phpversion']="PHP verzia ≥ %s";
+$plugin_tx['foldergallery']['syscheck_success']="Všetko v poriadku";
+$plugin_tx['foldergallery']['syscheck_title']="Kontrola systému";
+$plugin_tx['foldergallery']['syscheck_warning']="upozornenie";
+$plugin_tx['foldergallery']['syscheck_writable']="'%s' umožňuje zápis";
+$plugin_tx['foldergallery']['syscheck_xhversion']="CMSimple_XH verzia ≥ %s";
+$plugin_tx['foldergallery']['cf_thumb_size']="Výška náhľadov v pixeloch (px).";
+$plugin_tx['foldergallery']['cf_icon_format']="Formát ikony adresára. \"svg\" poskytuje lepšiu kvalitu na moderných smartfónoch, ale staršie prehliadače ju nepodporujú (napr. IE 8).";
