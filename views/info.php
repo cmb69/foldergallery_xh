180eebbe5056ca73943f8de1434efb4627af2d55
Christoph M. Becker <cmbecker69@gmx.de>
2017-12-01 01:43:12 +0100

    Use info screen styles of Pfw_XH

--- views/info.php
+++ views/info.php
@@ -1,22 +1,22 @@
 <h1>Foldergallery</h1>
-<img src="<?=$logo?>" class="foldergallery_logo" alt="<?=$this->text('alt_logo')?>">
+<img src="<?=$logo?>" class="pfw_logo" alt="<?=$this->text('alt_logo')?>">
 <p>Version: <?=$version?></p>
 <p>
     Copyright 2017 <a href="http://3-magi.net/">Christoph M. Becker</a>
 </p>
-<p class="foldergallery_license">
+<p class="pfw_license">
     Foldergallery_XH is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by the Free
     Software Foundation, either version 3 of the License, or (at your option)
     any later version.
 </p>
-<p class="foldergalley_license">
+<p class="pfw_license">
     Foldergallery_XH is distributed in the hope that it will be useful, but
     <em>without any warranty</em>; without even the implied warranty of
     <em>merchantability</em> or <em>fitness for a particular purpose</em>. See
     the GNU General Public License for more details.
 </p>
-<p class="foldergallery_license">
+<p class="pfw_license">
     You should have received a copy of the GNU General Public License along with
     Foldergallery_XH. If not, see <a
     href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
